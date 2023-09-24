ff373ea00d142405707feb30288342e2f40ea313
ietechennai <143881605+ietechennai@users.noreply.github.com>
2023-09-24 09:14:24 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,66 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: " What is the primary purpose of a patent?",
+    answer: "To protect trade secrets",
+    options: [
+      "To keep inventions a secret",
+      "To prevent others from using an invention",
+      "To promote open-source innovation",
+      "To protect trade secrets",
+    ],
+  },
+  {
+    numb: 2,
+    question:
+      "How long does a utility patent typically last from the date of filing?",
+    answer: "20 years",
+    options: ["5 years", "15 years", "20 years", "25 years"],
+  },
+  {
+    numb: 3,
+    question:
+      "Which type of patent covers ornamental designs for articles of manufacture?",
+    answer: "Design patent",
+    options: [
+      "Utility patent",
+      "Plant patent",
+      "Design patent",
+      "Process patent",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "What is the term for an invention that is new, useful, and not obvious to someone skilled in the field?",
+    answer: "Non-obviousness",
+    options: ["Novelty", "Obviousness", "Non-obviousness", "Non-Utility"],
+  },
+  {
+    numb: 5,
+    question:
+      "What is the process of reviewing a patent application by the patent office called?",
+    answer: "Patent examination",
+    options: [
+      "Patent examination",
+      "Patent grant",
+      "Patent enforcement",
+      "Patent licensing",
+    ],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ js/words.js
@@ -0,0 +1,10 @@
+const wordList = [
+  {
+    word: "patent",
+    hint: "Exclusive legal protection for new and useful inventions",
+  },
+  {
+    word: "fairuse",
+    hint: "A legal doctrine that allows limited use of copyrighted material without permission",
+  },
+];
